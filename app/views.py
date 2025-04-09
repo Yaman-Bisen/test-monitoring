30b46a1d102c5beaa58d211ccb3ac13636b57db7
yaman.bisen <yaman.bisen@easebuzz.in>
2025-04-09 22:17:21 +0530

    added create-user api

--- app/views.py
+++ app/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from .models import *
 # Create your views here.
+
+
+def index(request):
+    return JsonResponse({
+        'status': True,
+        'message': 'Hello world!!!'
+    })
+
+def create_user(request, number):
+    metrics = number % 10
+    message = 'DB operation failed.'
+    status_code = 400
+    status = False
+
+    if not metrics:
+        UserModel.objects.create(
+            username='test',
+            age=25
+        )
+        message= 'User created successfully.'
+        status_code = 200
+        status = True
+    
+    return JsonResponse({
+        'status': status,
+        'message': message
+    }, status_code=status_code)
--- test_project/urls.py
+++ test_project/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('prometheus/', include('django_prometheus.urls')),
+    path('', index),
+    path('create-user/<int:number>/', create_user)
 ]
